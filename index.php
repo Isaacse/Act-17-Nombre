410f39450fc7018efd0902f754225c21de994c27
Isaac Serrano <>
2023-03-29 13:57:06 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+      
+      $nombre = "Isaac Serrano de Leon";
+      echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//ES" ,
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+      echo '<html xmlns="http://www.w3.org/1999/xhtml">';
+      echo '<head>';
+      echo '<title>Actividad 17</title>';
+      echo '</head>';
+      echo '<body>';
+      echo '<p>';
+
+      echo "¡Hola $nombre! 😎<br/>";
+       echo 'Calle:Jesus Solorzano 🏡<p>';
+      echo 'Num de Casa:844#######</p>';
+      echo 'La fecha es 29 de Marzo del 2023.'; //comentario
+      echo 'Colonia:El Torreo<p>';
+      echo 'MunicipioSaltillo</p>';
+      echo '</body>';
+      echo '</html>';
+      ?> 
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
